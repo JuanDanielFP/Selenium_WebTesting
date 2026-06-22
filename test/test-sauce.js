16653ef2f613270579c7bce4ba12d5820c9b54bd
JuanDanielFP <juand21695@gmail.com>
2026-06-22 21:59:58 +0700

    update homework 10
    update codingan dengan penambahan hooks (before & after)

--- test/test-sauce.js
+++ test/test-sauce.js
@@ -1,43 +1,49 @@
+require('dotenv').config();
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const assert = require('assert');
+const chrome = require('selenium-webdriver/chrome');
+// const firefox = require('selenium-webdriver/firefox');
 
 describe('Sauce Labs Website', function() {
     let driver;
 
-    it('Visit Sauce Labs Website', async function() {
-        driver = await new Builder().forBrowser('chrome').build();
-        await driver.get('https://www.saucedemo.com'); 
-        
+    before(async function() {
+        let options = new chrome.Options();
+        // options.addArguments('--headless');
+        driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+        await driver.get(process.env.SAUCE_URL);
+
         let inputUsername = await driver.findElement(By.css('[data-test="username"]'));
         let inputPassword = await driver.findElement(By.xpath('//*[@data-test="password"]'));
         let buttonLogin = await driver.findElement(By.className('submit-button btn_action'));
-        await inputUsername.sendKeys('standard_user');
-        await inputPassword.sendKeys('secret_sauce');
+        await inputUsername.sendKeys(process.env.SAUCE_USERNAME);
+        await inputPassword.sendKeys(process.env.SAUCE_PASSWORD);
         await buttonLogin.click();
 
-        console.log('Login successful');
-        await driver.sleep(2000); // Wait for 2 seconds to observe the result
+        console.log('Setup and Login successful');
+        
+    });
+    after(async function() {
+        await driver.sleep(3000);
         await driver.quit();
     });
-    it('Sort products by name Descending', async function() {
-        driver = await new Builder().forBrowser('chrome').build();
-        await driver.get('https://www.saucedemo.com');
-        let inputUsername = await driver.findElement(By.css('[data-test="username"]'));
-        let inputPassword = await driver.findElement(By.xpath('//*[@data-test="password"]'));
-        let buttonLogin = await driver.findElement(By.className('submit-button btn_action'));
-        await inputUsername.sendKeys('standard_user');
-        await inputPassword.sendKeys('secret_sauce');
-        await buttonLogin.click();
 
-        console.log('Login successful');
-        
+    it('Visit Sauce Labs Website', async function() {
+        let inventoryContainer = await driver.findElement(By.id('inventory_container'));
+        assert.ok(inventoryContainer, 'Berhasil login dan masuk ke halaman inventory');
+        console.log('Verified on Dashboard');
+        await driver.sleep(1000);
+    });
+    it('Sort products by name Descending', async function() {
+
         let sortButton = await driver.findElement(By.className('product_sort_container'));
         await sortButton.click();
+        
+        await driver.sleep(1000);
         let sortOption = await driver.findElement(By.css('option[value="za"]'));
         await sortOption.click();
         
-        await driver.sleep(2000); // Wait for 2 seconds to observe the result
-        await driver.quit();
-    })
+        console.log('Produk berhasil di-sort secara Descending');
+    });
             
 });
